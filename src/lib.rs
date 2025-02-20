71fe17883fb203f378827471e71ebe214375cbb6
Henrik Tougaard <henrik@adaptagency.com>
2025-02-20 10:13:55 +0100

    Update to Axum 0.8 and edition 2024
    * Upgrades:
      - Axum to 0.8
      - Opentelemetry to 0.29
      - Rust edition to 2024
      - and all other dependencies to latest
    * Use feature `reqwest-blocking-client` in opentelemetry, as that seems to give fever problems.

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,8 @@ use opentelemetry::trace::TracerProvider as _;
 use opentelemetry_otlp::{LogExporter, SpanExporter};
 use opentelemetry_sdk::{
     self as sdk,
-    logs::{Logger, LoggerProvider},
-    trace::{Config, Sampler, Tracer},
+    logs::{SdkLogger, SdkLoggerProvider},
+    trace::{Sampler, Tracer},
 };
 use tracing_core::Subscriber;
 use tracing_opentelemetry::OpenTelemetryLayer;
@@ -33,9 +33,9 @@ pub use axum_layer::{opentelemetry_tracing_layer, opentelemetry_tracing_layer_wi
 ///
 /// Expects the following environment variables:
 /// *  `HONEYCOMB_API_KEY` contains
-///     the API key for the Honeycomb environment that traces should be sent to
+///    the API key for the Honeycomb environment that traces should be sent to
 /// *  `OTEL_EXPORTER_OTLP_ENDPOINT` contains the endpoint for Honeycomb -
-///     eg `https://api.eu1.honeycomb.io/`
+///    eg `https://api.eu1.honeycomb.io/`
 /// *  `OTEL_SERVICE_NAME` contains the service name - eg `clap::crate_name!()`.
 pub fn init_otlp_layer<S>(sample_rate: f64) -> Option<OpenTelemetryLayer<S, Tracer>>
 where
@@ -46,13 +46,11 @@ where
     );
 
     if let Ok(exporter) = SpanExporter::builder().with_http().build() {
-        let provider = sdk::trace::TracerProvider::builder()
-            .with_batch_exporter(exporter, opentelemetry_sdk::runtime::Tokio)
-            .with_config(
-                Config::default().with_sampler(Sampler::ParentBased(Box::new(
-                    Sampler::TraceIdRatioBased(sample_rate),
-                ))),
-            )
+        let provider = sdk::trace::SdkTracerProvider::builder()
+            .with_batch_exporter(exporter)
+            .with_sampler(Sampler::ParentBased(Box::new(Sampler::TraceIdRatioBased(
+                sample_rate,
+            ))))
             .build();
         let tracer = provider.tracer("axum-otlp-honeycomb");
         let layer = tracing_opentelemetry::layer()
@@ -80,10 +78,10 @@ where
 /// field. Any field in the event with the name `body` will overwrite the event message.
 ///
 /// Expects the same environment variables as `init_otlp_log_layer()`
-pub fn init_otlp_log_layer() -> AxumOtelEventLogger<LoggerProvider, Logger> {
+pub fn init_otlp_log_layer() -> AxumOtelEventLogger<SdkLoggerProvider, SdkLogger> {
     let exporter = LogExporter::builder().with_http().build().unwrap();
-    let provider = sdk::logs::LoggerProvider::builder()
-        .with_batch_exporter(exporter, opentelemetry_sdk::runtime::Tokio)
+    let provider = sdk::logs::SdkLoggerProvider::builder()
+        .with_batch_exporter(exporter)
         .build();
     AxumOtelEventLogger::new(&provider)
 }
